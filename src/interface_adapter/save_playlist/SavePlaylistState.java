4572387d0e98e6f7c95c62814a80c2567493622d
Mahek Cheema <mahek.cheema@mail.utoronto.ca>
2023-11-18 15:58:21 -0500

    implemented save playlist use case and extended complete playlist entity as required by use case

--- src/interface_adapter/save_playlist/SavePlaylistState.java
+++ src/interface_adapter/save_playlist/SavePlaylistState.java
@@ -1,4 +1,22 @@
 package interface_adapter.save_playlist;
 
 public class SavePlaylistState {
+    private String filePath;
+    private String error;
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
--- src/use_case/save_playlist/SavePlaylistInputBoundary.java
+++ src/use_case/save_playlist/SavePlaylistInputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.save_playlist;
 
 public interface SavePlaylistInputBoundary {
+    // Execute function that takes in the input data and calls the interactor
+    void execute(SavePlaylistInputData inputData);
 }
