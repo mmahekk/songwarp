fc585580a0a2b50cc9a1d1b93bb947e41af9fa3c
wilson0516 <xinzewang516@gmail.com>
2023-11-21 16:19:45 -0500

    just updating my branch so i continue on my pc instead of laptop

--- src/interface_adapter/load_playlist/LoadPlaylistController.java
+++ src/interface_adapter/load_playlist/LoadPlaylistController.java
@@ -1,4 +1,15 @@
 package interface_adapter.load_playlist;
 
+import use_case.load_playlist.LoadPlaylistInputBoundary;
+
 public class LoadPlaylistController {
+    final LoadPlaylistInputBoundary loadPlaylistUseCaseInteractor;
+
+    public LoadPlaylistController(LoadPlaylistInputBoundary loadPlaylistUseCaseInteractor) {
+        this.loadPlaylistUseCaseInteractor = loadPlaylistUseCaseInteractor;
+    }
+
+    public void execute() {
+
+    }
 }
--- src/use_case/load_playlist/LoadPlaylistOutputData.java
+++ src/use_case/load_playlist/LoadPlaylistOutputData.java
@@ -1,4 +1,15 @@
 package use_case.load_playlist;
 
+import entity.Playlist;
+
 public class LoadPlaylistOutputData {
+    private final Playlist playlist;
+
+    public LoadPlaylistOutputData(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    public Playlist getPlaylist() {
+        return this.playlist;
+    }
 }
