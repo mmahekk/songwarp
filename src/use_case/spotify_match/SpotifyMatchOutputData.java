8aac10b130ae65ccabb5ce5cd8eff628427039f7
Abhi <113407740+abhilo@users.noreply.github.com>
2023-11-24 16:08:45 -0500

    updated

--- src/use_case/spotify_match/SpotifyMatchOutputData.java
+++ src/use_case/spotify_match/SpotifyMatchOutputData.java
@@ -1,4 +1,16 @@
 package use_case.spotify_match;
 
+import entity.CompletePlaylist;
+
 public class SpotifyMatchOutputData {
+
+    private final CompletePlaylist playlist;
+
+    public SpotifyMatchOutputData(CompletePlaylist playlist) {
+        this.playlist = playlist;
+    }
+
+    public CompletePlaylist getPlaylist() {
+        return playlist;
+    }
 }
